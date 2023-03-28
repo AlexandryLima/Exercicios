32cfea2b2e54c787aa86a1f3c41c6225b6383a82
teste <alexandryqueirozlima@gmail.com>
2023-03-28 20:42:41 -0300

    Introdução ao Kotlin

---
+++ src/main/kotlin/IntroductionKotlin.kt
@@ -0,0 +1,6 @@
+fun main (){
+    println("Qual a diferença de um arquivo java para um arquivo kotlin?")
+    println("Por mais que sejam utilizados para as mesmas coisas, as linguagens se diferenciam " +
+            "por muitos aspectos, porém o mais importante é de que a linguagem Kotlin é uma das linguagens " +
+            "principais para programação mobile (android) segundo o Google.")
+}
