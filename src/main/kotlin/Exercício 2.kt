4cce4c61584327a743d2723e6aa2171770a9d66b
teste <alexandryqueirozlima@gmail.com>
2023-03-28 20:42:41 -0300

    Exercício 2

--- src/main/kotlin/Exercício 2.kt
+++ src/main/kotlin/Exercício 2.kt
@@ -3,6 +3,6 @@ fun main (){
     val n2: Int= 5
     println(n1)
     println(n2)
-    // o valor 10 é uma variável pois pode ser mudada durante a execução do programa
-    // o valor 5 é uma constante pois ele será imutável durante a execução do programa
+    // o valor 10 (var) é uma variável pois pode ser mudada durante a execução do programa
+    // o valor 5 (val) é uma constante pois ele será imutável durante a execução do programa
 }
