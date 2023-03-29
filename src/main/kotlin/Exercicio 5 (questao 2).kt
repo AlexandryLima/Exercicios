ce39d9f02ab6799c7ab9ce47d8685b24084bf031
teste <alexandryqueirozlima@gmail.com>
2023-03-29 20:38:13 -0300

    Exercício 5 (Questão 2)

---
+++ src/main/kotlin/Exercicio 5 (questao 2).kt
@@ -0,0 +1,8 @@
+fun main(){
+    println("Digite o valor pelo qual deseja obter o valor quadrado: ")     // Questâo 2
+    var n1: Int = readln().toInt()
+
+    var result:Int = (n1*n1).toInt()
+
+    println("O valor do quadrado é: $result")
+}
