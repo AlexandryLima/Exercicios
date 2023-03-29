d1e869827d1f88ebbab8b26c2b041ef86caea08b
teste <alexandryqueirozlima@gmail.com>
2023-03-29 16:25:11 -0300

    Exercício 4

---
+++ src/main/kotlin/Exercício 4.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var n1: Int= 5                              // Questão a)
+    var n2: Int=12
+    var result: Int= (n1+n2)
+
+    println(result)
+
+    var verdadeiro: Boolean = 5 > 3 && 2 <      // Questão b)
+
+    var falso: Boolean=  !(10 > 5 || 3 < 1)     // Questâo c)
+
+
+    println(verdadeiro)
+    println(falso)
+}
