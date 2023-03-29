0372f8e61618dcc24c24dfcbfa83ee36f1bcabc7
teste <alexandryqueirozlima@gmail.com>
2023-03-29 20:45:26 -0300

    Exercício 5 (Questão 3)

---
+++ src/main/kotlin/Exercício 5 (Questão 3).kt
@@ -0,0 +1,12 @@
+fun main(){
+    println("Olá, digite o seu nome: ")
+    var name: String = readln()
+
+    println("Agora digite a sua idade em anos :")
+    var idade: Int = readln().toInt()
+
+    println("Olá $name, seja bem-vindo!")
+    println("Você possui $idade anos de idade.")
+
+
+}
