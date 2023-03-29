9317d267c2038f55effc54b30022e1cbc94fe501
teste <alexandryqueirozlima@gmail.com>
2023-03-29 16:14:57 -0300

    Exercício 3

---
+++ src/main/kotlin/Exercício 3.kt
@@ -0,0 +1,13 @@
+fun main (){
+    var decimal: Double = 5.158         //Utilizo "double" ou "Float" para decimais
+    println(decimal)
+
+    var text: String="Alo"              // Utilizo "String" para textos ou palavras
+    println(text)
+
+    var inteiro: Int= (510)             // Utilizo "Byte, Short, Int e Long" para valores inteiros
+    println(inteiro)
+
+
+
+}
