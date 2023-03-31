eb00c61516b0fe8feffae48d20c1e605985c7dcb
teste <alexandryqueirozlima@gmail.com>
2023-03-31 15:32:03 -0300

    Exercício 8 (Questão 3)

---
+++ src/main/kotlin/Exercico 8 ( Questão 3).kt
@@ -0,0 +1,24 @@
+fun main(){
+    println("Digite um número de 1 ao 12 para saber o mês correspodente")
+    var number: Int = readln().toInt()
+    when (number){
+
+        1 -> println("Janeiro")
+        2 -> println("Fevereiro")
+        3 -> println("Março")
+        4 -> println("Abril")
+        5 -> println("Maio")
+        6 -> println("Junho")
+        7 -> println("Julho")
+        8 -> println("Agosto")
+        9 -> println("Setembro")
+        10 -> println("Outubro")
+        11 -> println("Novembro")
+        12 -> println("Dezembro")
+
+
+    }
+
+
+
+}
