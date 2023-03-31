537304e8316b75490ce6f84ef51491663a4ee25c
teste <alexandryqueirozlima@gmail.com>
2023-03-31 15:41:14 -0300

    Exercício 8 (Questão 3)

---
+++ src/main/kotlin/Exercicio 8 (Questão 3).kt
@@ -0,0 +1,9 @@
+fun main(){
+    println("Digite uma letra")
+    var letra:String= readln()
+    if (letra == "a" || letra == "e" || letra == "i" || letra =="o" || letra == "u"){
+        println("essa letra é uma vogal")
+    }else{
+        println("Essa letra  é uma consoante")
+    }
+}
